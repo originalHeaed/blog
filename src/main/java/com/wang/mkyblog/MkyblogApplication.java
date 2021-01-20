576ffee9ebba49e4a0473f256fa3095ff3350801
ComeOnWang <1879519397@qq.com>
2021-01-20 19:35:40 +0800

    2020-1-20提交日志

---
+++ src/main/java/com/wang/mkyblog/MkyblogApplication.java
@@ -0,0 +1,13 @@
+package com.wang.mkyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkyblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkyblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wang/mkyblog/MkyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.mkyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
